b1b6a725713f28e1e7edd91e14b5e7fe5263ef11
t1bor <tibor.kecskemeti@logmein.com>
2015-05-10 15:31:11 +0200

    returning from function before it makes trouble

--- script/carousel.js
+++ script/carousel.js
@@ -6,20 +6,19 @@ var width = items.querySelector("li").getBoundingClientRect().width;
 
 var left = 0;
 
-/*
-var clickCountLeft = 0;
-var clickCountRight = 0;
-var itemsCount = items.childNodes;
-*/
-/* divide itemsCount by two as it counts both <li> and <p> */
-
 window.addEventListener("resize",updateWidth);
 prev.addEventListener("click",rollLeft);
 next.addEventListener("click",rollRight);
 
 function updateWidth() {
   width = items.querySelector("li").getBoundingClientRect().width;
+  console.log("width changed");
+  /*
+  items.style.left = 0;
+  left = 0;
+  */
 }
+
 
 console.log("width times 3 = " + width*3);
 
@@ -28,21 +27,21 @@ function rollLeft() {
   if (left == 0) {
     prev.style.display = "none";
     next.style.display = "inline-block";
+    return;
   }
 
-  if ((Math.abs(left) < width*3) && left !=0 ) {
+  if (Math.abs(left) <= width*3) {
     left = left + width;
     items.style.left = left + "px";
   }
-  console.log(Math.abs(left));
 }
 
 function rollRight() {
 
   if (Math.abs(left) == (width*3)) {
-    left = 0;
     next.style.display = "none";
     prev.style.display = "inline-block";
+    return;
   }
 
   if (Math.abs(left) < width*3 && left != width*3 ) {
@@ -51,31 +50,3 @@ function rollRight() {
   }
   console.log(Math.abs(left));
 }
-
-/* Click count solution for The Hall of Fame
-
-function rollLeft(){
-  clickCountRight = 0;
-  clickCountLeft += 2.5;
-  if (clickCountLeft < (itemsCount.length)) {
-    left = left + width;
-    items.style.left = left + "px";
-  }
-  console.log(itemsCount.length);
-  console.log(clickCountLeft);
-  console.log(left);
-}
-
-function rollRight(){
-  clickCountLeft = 0;
-  clickCountRight += 2.5;
-  if (clickCountRight < (itemsCount.length)) {
-  left = left - width;
-  items.style.left = left + "px";
-  }
-  console.log(itemsCount.length);
-  console.log(clickCountRight);
-  console.log(left);
-}
-
-*/
